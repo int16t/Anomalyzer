254990c70a212467fc98776f9fabfba1a5bf52bd
int16t <gy4ra@protonmail.com>
2025-11-06 09:56:55 -0300

    feat: fixing directory errors

--- src/principal.py
+++ src/principal.py
@@ -1,9 +1,12 @@
+import os
+from pathlib import Path
 from preprocessamento import preprocess
 from treinamento import treinar_modelo, salvar_modelo
 
-
-train_path = '../data/NSL_KDD_Train.csv'
-test_path = '../data/NSL_KDD_Test.csv'
+# Obtém o diretório do arquivo atual e navega até a pasta data
+BASE_DIR = Path(__file__).resolve().parent.parent
+train_path = BASE_DIR / 'data' / 'NSL_KDD_Train.csv'
+test_path = BASE_DIR / 'data' / 'NSL_KDD_Test.csv'
 
 X_train, y_train, X_test, y_test, scaler = preprocess(train_path, test_path)
 
